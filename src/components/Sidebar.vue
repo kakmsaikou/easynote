df7790133fea58375f1ffd57882ecff1bd4f29dd
TacitusPie <kakmsaikou@gmail.com>
2022-08-24 15:22:29 +0800

    添加笔记本 router-link

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -5,6 +5,9 @@
       <router-link to="/notebook/" title="笔记">
         <i class="iconfont icon-note"></i>
       </router-link>
+      <router-link to="/notebooks" title="笔记本">
+        <i class="iconfont icon-notebook"></i>
+      </router-link>
       <router-link to="/trash/" title="回收站">
         <i class="iconfont icon-trash"></i>
       </router-link>
