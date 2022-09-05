9e27e1121c977c6ddd17984fe7ec5dda5f3d394a
TacitusPie <kakmsaikou@gmail.com>
2022-09-05 11:13:49 +0800

    优化路由

--- src/router/index.js
+++ src/router/index.js
@@ -25,11 +25,11 @@ export default new Router({
       component: Notebooks
     },
     {
-      path: '/note/:noteId',
+      path: '/note',
       component: Note
     },
     {
-      path: '/trash/:noteId',
+      path: '/trash',
       component: Trash
     }
   ]
